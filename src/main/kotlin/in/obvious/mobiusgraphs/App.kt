981564e5eaa5d4ee6892ef2a2fb26557303f368c
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-04-11 15:42:36 +0530

    Add Swing GUI to run on main execution

--- src/main/kotlin/in/obvious/mobiusgraphs/App.kt
+++ src/main/kotlin/in/obvious/mobiusgraphs/App.kt
@@ -1,5 +1,19 @@
 package `in`.obvious.mobiusgraphs
 
+import java.awt.Dimension
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.SwingConstants
+
 fun main(args: Array<String>) {
     logger().debug("Run with args: ${args.joinToString()}")
+
+    with(JFrame("Mobius Graphs")) {
+        size = Dimension(640, 480)
+        add(JLabel("Hello, World!", SwingConstants.CENTER))
+
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
+        isVisible = true
+    }
 }
