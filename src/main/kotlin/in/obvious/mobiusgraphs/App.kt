3a7d4bea91e17f4c00845605ef11d91348a444d9
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-04-11 16:17:58 +0530

    Render sample graph in GUI

--- src/main/kotlin/in/obvious/mobiusgraphs/App.kt
+++ src/main/kotlin/in/obvious/mobiusgraphs/App.kt
@@ -1,19 +1,53 @@
 package `in`.obvious.mobiusgraphs
 
+import guru.nidi.graphviz.attribute.Color
+import guru.nidi.graphviz.attribute.Rank
+import guru.nidi.graphviz.attribute.Style
+import guru.nidi.graphviz.engine.Format
+import guru.nidi.graphviz.engine.Graphviz
 import java.awt.Dimension
 import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.SwingConstants
+import guru.nidi.graphviz.model.Factory.*
+import java.awt.image.BufferedImage
+import javax.swing.ImageIcon
 
 fun main(args: Array<String>) {
     logger().debug("Run with args: ${args.joinToString()}")
 
+    val graphGenerator = GraphGenerator()
+
     with(JFrame("Mobius Graphs")) {
-        size = Dimension(640, 480)
-        add(JLabel("Hello, World!", SwingConstants.CENTER))
+        val image = graphGenerator.generateAsImage()
+
+        size = Dimension(image.width + 50, image.height + 50)
+
+        add(JLabel(ImageIcon(image), SwingConstants.CENTER))
 
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
         isVisible = true
+    }
+}
+
+class GraphGenerator {
+
+    fun generateAsImage(): BufferedImage {
+        val graph = graph("Sample")
+            .directed()
+            .graphAttr()
+            .with(Rank.dir(Rank.RankDir.LEFT_TO_RIGHT))
+            .with(
+                node("a").with(Color.RED).link(node("b")),
+                node("b").link(node("c")).with(Style.DASHED),
+                node("c")
+            )
+
+        return Graphviz
+            .fromGraph(graph)
+            .height(100)
+            .render(Format.SVG)
+            .toImage()
     }
 }
