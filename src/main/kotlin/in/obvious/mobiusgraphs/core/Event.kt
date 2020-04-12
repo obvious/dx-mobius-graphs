a129b587223ec0854dc29312d0bfcb0533fc917d
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-04-12 16:28:15 +0530

    Change `Event` to be a sealed class
    Types:
    - `InternalEvent`: events that are generated via effects
    - `ExternalEvent`: events that are generated externally

--- src/main/kotlin/in/obvious/mobiusgraphs/core/Event.kt
+++ src/main/kotlin/in/obvious/mobiusgraphs/core/Event.kt
@@ -1,3 +1,13 @@
 package `in`.obvious.mobiusgraphs.core
 
-data class Event(val name: String)
+sealed class Event(val name: String)
+
+data class InternalEvent(
+    private val _name: String,
+    val source: Effect
+): Event(_name)
+
+data class ExternalEvent(
+    private val _name: String,
+    val source: State
+): Event(_name)
