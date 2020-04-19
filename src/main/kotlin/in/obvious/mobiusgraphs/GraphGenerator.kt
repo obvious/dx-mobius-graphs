4578724230a30b462b093f954c22c12585ff5a78
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-04-19 11:40:17 +0530

    Refactor methods in `GraphGenerator`

--- src/main/kotlin/in/obvious/mobiusgraphs/GraphGenerator.kt
+++ src/main/kotlin/in/obvious/mobiusgraphs/GraphGenerator.kt
@@ -1,88 +1,35 @@
 package `in`.obvious.mobiusgraphs
 
 import `in`.obvious.mobiusgraphs.core.*
+import com.google.common.graph.EndpointPair
 import com.google.common.graph.Network
 import guru.nidi.graphviz.attribute.*
 import guru.nidi.graphviz.engine.Format
 import guru.nidi.graphviz.engine.Graphviz
 import guru.nidi.graphviz.model.Factory
+import guru.nidi.graphviz.model.Link
 import guru.nidi.graphviz.model.MutableNode
 import java.awt.image.BufferedImage
 
+@Suppress("UnstableApiUsage")
 class GraphGenerator {
 
-    @Suppress("UnstableApiUsage")
     fun generate(
         logicGraph: Network<State, Event>,
         name: String
     ): BufferedImage {
-        val nodes: Map<String, MutableNode> = logicGraph
-            .nodes()
-            .map { state ->
-                val nameLine = "<b>${state.name}</b>"
-                val effectLine = if (state.effects.isEmpty()) "" else "<i>${state.effects.joinToString { it.name }}</i>"
+        val nodes: Map<String, MutableNode> = constructNodes(logicGraph)
 
-                val node = when (state) {
-                    is Concrete -> {
-                        val label = if (effectLine.isNotBlank()) Label.htmlLines(
-                            Label.Justification.MIDDLE,
-                            nameLine,
-                            effectLine
-                        ) else Label.htmlLines(
-                            Label.Justification.MIDDLE,
-                            nameLine
-                        )
-                        Factory.mutNode(label)
-                    }
-                    is Intermediate -> {
-                        val label = Label.htmlLines(
-                            Label.Justification.MIDDLE,
-                            effectLine
-                        )
-                        Factory.mutNode(label)
-                            .add(Style.DASHED)
-                            .add(Color.DIMGRAY)
-                    }
-                }
-                state.name to node
-            }
-            .associateBy({ (name, _) -> name }, { (_, node) -> node })
-
-        logicGraph
-            .edges()
-            .map { it to logicGraph.incidentNodes(it) }
-            .map { (event, endpoint) ->
-                val from = endpoint.source()
-                val to = endpoint.target()
-
-                val sourceNode = nodes.getValue(from.name)
-                val targetNode = nodes.getValue(to.name)
-
-                val edgeName = when (event) {
-                    is ExternalEvent -> Label.of(
-                        event.name
-                    )
-                    is InternalEvent -> Label.of(
-                        "${event.source.name}: ${event.name}"
-                    )
-                }
-                val link = sourceNode.linkTo(targetNode).with(edgeName)
-
-                if (event is InternalEvent) {
-                    link.add(Color.NAVY).add(Color.NAVY.font())
-                }
-
-                sourceNode to link
-            }
-            .onEach { (from, link) -> from.addLink(link) }
-            .map { it.second }
+        linkNodesWithEdges(nodes, logicGraph)
 
         val graph = Factory.mutGraph().setDirected(true)
+
         graph.graphAttrs().apply {
             add(Rank.dir(Rank.RankDir.TOP_TO_BOTTOM))
             add(
                 Label.html("<b>$name</b>")
-                    .locate(Label.Location.TOP))
+                    .locate(Label.Location.TOP)
+            )
         }
         graph.nodeAttrs().add(Shape.ELLIPSE)
 
@@ -92,5 +39,84 @@ class GraphGenerator {
             .height(1000)
             .render(Format.SVG)
             .toImage()
+    }
+
+    private fun linkNodesWithEdges(
+        nodes: Map<String, MutableNode>,
+        logicGraph: Network<State, Event>
+    ) {
+        logicGraph
+            .edges()
+            .map { it to logicGraph.incidentNodes(it) }
+            .map { (event, endpoint) ->
+                mapEdgesToLinks(
+                    endpoint,
+                    event,
+                    findNodeByName = { stateName -> nodes.getValue(stateName) }
+                )
+            }
+            .forEach { (from, link) -> from.addLink(link) }
+    }
+
+    private fun constructNodes(logicGraph: Network<State, Event>): Map<String, MutableNode> {
+        return logicGraph
+            .nodes()
+            .map { state -> state.name to mapStateToNode(state) }
+            .associateBy({ (name, _) -> name }, { (_, node) -> node })
+    }
+
+    private fun mapEdgesToLinks(
+        endpoint: EndpointPair<State>,
+        event: Event,
+        findNodeByName: (String) -> MutableNode
+    ): Pair<MutableNode, Link> {
+        val from = endpoint.source()
+        val to = endpoint.target()
+
+        val sourceNode = findNodeByName(from.name)
+        val targetNode = findNodeByName(to.name)
+
+        val edgeName = when (event) {
+            is ExternalEvent -> Label.of(
+                event.name
+            )
+            is InternalEvent -> Label.of(
+                "${event.source.name}: ${event.name}"
+            )
+        }
+        val link = sourceNode.linkTo(targetNode).with(edgeName)
+
+        if (event is InternalEvent) {
+            link.add(Color.NAVY).add(Color.NAVY.font())
+        }
+        return Pair(sourceNode, link)
+    }
+
+    private fun mapStateToNode(state: State): MutableNode {
+        val nameLine = "<b>${state.name}</b>"
+        val effectLine = if (state.effects.isEmpty()) "" else "<i>${state.effects.joinToString { it.name }}</i>"
+
+        return when (state) {
+            is Concrete -> {
+                val label = if (effectLine.isNotBlank()) Label.htmlLines(
+                    Label.Justification.MIDDLE,
+                    nameLine,
+                    effectLine
+                ) else Label.htmlLines(
+                    Label.Justification.MIDDLE,
+                    nameLine
+                )
+                Factory.mutNode(label)
+            }
+            is Intermediate -> {
+                val label = Label.htmlLines(
+                    Label.Justification.MIDDLE,
+                    effectLine
+                )
+                Factory.mutNode(label)
+                    .add(Style.DASHED)
+                    .add(Color.DIMGRAY)
+            }
+        }
     }
 }
